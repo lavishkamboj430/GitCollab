5c14675eb6f6f95fded5962aee94834fa81c057c
lavish <lavishkamboj430@gmail.com>
2026-09-22 11:34:16 +0530

    Controller and Routes are Created

---
+++ Controllers/Controller.js
@@ -0,0 +1,19 @@
+const UserInfo = async (req, res) => {
+    const Data = req.body
+
+    if (!Data) {
+        return res.status(400).json(
+            {
+                Message: "No Data Found"
+            }
+        )
+    }
+
+
+    return res.status(200).json(
+        {
+            Messgae: "User Info",
+            Data: Data
+        }
+    )
+}
